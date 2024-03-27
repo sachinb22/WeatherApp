1a02467335d301982eead299880f454e4f3f265f
Sachin Basnet <sbasnet@uef.fi>
2024-03-27 12:43:34 +0200

    style has been added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './App.css'
 import WeatherApp from './components/WeatherApp'
 
 function App() {
--- src/components/WeatherApp.jsx
+++ src/components/WeatherApp.jsx
@@ -14,11 +14,32 @@ const WeatherApp = () => {
     <>
     <div className="container">
         <div className="top-bar">
-            <input type="text" className="cityInput" />
+            <input type="text" className="cityInput" placeholder='Search' />
             <div className="search-icon">
                 <img src={search_icon} alt="" />
-            </div>
+            </div>            
         </div>
+            <div className="weather-image">
+                <img src={cloud_icon} alt="" />
+            </div>
+            <div className="weather-temp"> 24°C </div>
+            <div className="weather-location">Helsinki</div>
+            <div className="data-container">
+                <div className="element">
+                    <img src={humidity_icon} alt="" className="icon" />
+                    <div className="data">
+                        <div className="humidity-percent">64%</div>
+                        <div className="text">Humidity</div>
+                    </div>
+                </div>
+                <div className="element">
+                    <img src={wind_icon} alt="" className="icon" />
+                    <div className="data">
+                        <div className="humidity-percent">18 Km/hr</div>
+                        <div className="text">Wind Speed</div>
+                    </div>
+                </div>
+            </div>
     </div>
     </>
   )
