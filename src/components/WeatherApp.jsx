759423da416b7c8c6e8f71becfcbb71d44fe9073
Sachin Basnet <sbasnet@uef.fi>
2024-03-27 17:57:16 +0200

    api intergration

--- src/components/WeatherApp.jsx
+++ src/components/WeatherApp.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './WeatherApp.css'
 import clear_icon from '../assets/clear.png'
 import cloud_icon from '../assets/cloud.png'
@@ -9,33 +9,93 @@ import search_icon from '../assets/search.png'
 import snow_icon from '../assets/snow.png'
 import wind_icon from '../assets/wind.png'
 
+
 const WeatherApp = () => {
+
+    let api_key= "5eaa79c9739c4fe79814209b802e6b91";
+
+    const [wicon, setWicon] = useState(cloud_icon)
+
+    const search = async ()=> {
+        const element = document.getElementsByClassName('cityInput')
+        if(element[0].value === "")
+        {
+            return 0;
+        }
+
+         let url = `https://api.openweathermap.org/data/2.5/weather?q=${element[0].value}&units=Metric&appid=${api_key}`
+
+        let response = await fetch(url);
+        let data = await response.json();
+
+        const humidity = document.getElementsByClassName('humidity-percent');
+        const wind = document.getElementsByClassName('wind-rate');
+        const temperature = document.getElementsByClassName('weather-temp');
+        const location = document.getElementsByClassName('weather-location');
+
+        humidity[0].innerHTML = Math.floor(data.main.humidity) + '%';
+        wind[0].innerHTML = Math.floor(data.wind.speed) + 'km/h';
+        temperature[0].innerHTML = Math.floor(data.main.temp) + '°C';
+        location[0].innerHTML = data.name;
+
+
+        if(data.weather[0].icon === "01d" || data.weather[0].icon === "01n" )
+        {
+            setWicon(clear_icon)
+        }
+        else if(data.weather[0].icon === "02d" || data.weather[0].icon === "02n" )
+        {
+            setWicon(cloud_icon)
+        }
+        else if(data.weather[0].icon === "03d" || data.weather[0].icon === "03n" )
+        {
+            setWicon(drizzle_icon)
+        }
+        else if(data.weather[0].icon === "04d" || data.weather[0].icon === "04n" )
+        {
+            setWicon(drizzle_icon)
+        }
+        else if(data.weather[0].icon === "10d" || data.weather[0].icon === "10n" )
+        {
+            setWicon(rain_icon)
+        }
+        else if(data.weather[0].icon === "13d" || data.weather[0].icon === "13n" )
+        {
+            setWicon(snow_icon)
+        }
+        else {
+            setWicon(clear_icon)
+        }
+
+
+    }
+
   return (
     <>
     <div className="container">
         <div className="top-bar">
             <input type="text" className="cityInput" placeholder='Search' />
-            <div className="search-icon">
+            <div className="search-icon" onClick={()=>{search()}}>
                 <img src={search_icon} alt="" />
             </div>            
         </div>
             <div className="weather-image">
-                <img src={cloud_icon} alt="" />
+                <img src={wicon} alt="" />
             </div>
-            <div className="weather-temp"> 24°C </div>
+            <div className="weather-temp">  </div>
             <div className="weather-location">Helsinki</div>
             <div className="data-container">
                 <div className="element">
                     <img src={humidity_icon} alt="" className="icon" />
                     <div className="data">
-                        <div className="humidity-percent">64%</div>
+                        <div className="humidity-percent"></div>
                         <div className="text">Humidity</div>
                     </div>
                 </div>
                 <div className="element">
                     <img src={wind_icon} alt="" className="icon" />
                     <div className="data">
-                        <div className="humidity-percent">18 Km/hr</div>
+                        <div className="wind-rate"></div>
                         <div className="text">Wind Speed</div>
                     </div>
                 </div>
